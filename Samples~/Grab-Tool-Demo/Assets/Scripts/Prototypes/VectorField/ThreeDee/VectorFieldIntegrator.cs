8e1dbd06201ae6b33946c4d3aba362e11f326ef6
Matt Bishop <mattsbishop1@gmail.com>
2024-02-01 09:01:47 -0700

    Preserving 2D vector field integration, getting started on 3D.

--- Samples~/Grab-Tool-Demo/Assets/Scripts/Prototypes/VectorField/ThreeDee/VectorFieldIntegrator.cs
+++ Samples~/Grab-Tool-Demo/Assets/Scripts/Prototypes/VectorField/ThreeDee/VectorFieldIntegrator.cs
@@ -1,45 +1,46 @@
-using System;
 using System.Linq;
 using GrabTool.Math;
+using Prototypes.VectorField.TwoDimensional;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace Prototypes.VectorField.TwoDimensional
+namespace Prototypes.VectorField.ThreeDee
 {
-    [RequireComponent(typeof(ParticleContainer), typeof(Grid))]
     public class VectorFieldIntegrator : MonoBehaviour
     {
         [Tooltip("The outer loop cutoff")] [SerializeField]
         private float rO;
+
         [Tooltip("The inner loop cutoff")] [SerializeField]
         private float rI;
+
         [Tooltip("Multiplier for underlying r function")] [SerializeField] [Range(0.1f, 4.0f)]
         private float rMultiplier = 0.1f;
-        
-        private Grid _grid;
+
+        private TwoDimensional.Grid _grid;
         [SerializeField] private bool showGridVisualization;
 
         private ParticleContainer _container;
         [SerializeField] private bool updatePositions;
 
-        private readonly VectorField2D _vectorField2D = new();
+        private readonly VectorField3D _vectorField3D = new();
 
         private bool doUpdate;
 
         private float d;
-        
+
         private void Start()
         {
             _container = GetComponent<ParticleContainer>();
-            _grid = GetComponent<Grid>();
+            _grid = GetComponent<TwoDimensional.Grid>();
         }
 
         private void Update()
         {
-            _vectorField2D.Ri = rI;
-            _vectorField2D.Ro = rO;
-            _vectorField2D.RMultiplier = rMultiplier;
-            
+            _vectorField3D.Ri = rI;
+            _vectorField3D.Ro = rO;
+            _vectorField3D.RMultiplier = rMultiplier;
+
             Assert.IsTrue(rI < rO);
 
             if (showGridVisualization)
@@ -47,7 +48,7 @@ namespace Prototypes.VectorField.TwoDimensional
                 _grid.enabled = true;
                 foreach (var v in _grid.Points.Select((v, i) => new { v, i }))
                 {
-                    var value = _vectorField2D.GetVelocity(v.v.x, v.v.y);
+                    var value = _vectorField3D.GetVelocity(v.v);
                     _grid.Velocities[v.i] = value;
                     _grid.Colors[v.i] = Color.blue;
                 }
@@ -60,31 +61,31 @@ namespace Prototypes.VectorField.TwoDimensional
 
         public void HandleMouseMove((Vector3 DesiredTranslation, Vector3 Center) input)
         {
-            _vectorField2D.C = input.Center;
-            _vectorField2D.DesiredTranslation = input.DesiredTranslation;
+            _vectorField3D.C = input.Center;
+            _vectorField3D.DesiredTranslation = input.DesiredTranslation;
             d = Vector3.Magnitude(input.DesiredTranslation);
             doUpdate = true;
         }
-        
+
         private void FixedUpdate()
         {
             if (!doUpdate) return;
-            
+
             foreach (var particle in _container.Particles)
             {
                 var position = particle.position;
-                var v = _vectorField2D.GetVelocity(position.x, position.y);
+                var v = _vectorField3D.GetVelocity(position);
 
                 if (v.magnitude == 0) continue;
-                
+
                 var t = d / v.magnitude;
-                
+
                 // Debug.Log($"Time update is {t}");
 
                 if (updatePositions)
                 {
                     position += t * new Vector3(v.x, v.y, 0);
-                
+
                     particle.position = position;
                 }
             }
